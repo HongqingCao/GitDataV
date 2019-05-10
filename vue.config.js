ad73afe7ed0990dfce5d98124cdf1af3af37ae95
caohongqing <caohongqing@weyao.com>
2019-05-10 18:43:14 +0800

    Edit vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/word-clock" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/GitDataV" : "/",
   devServer: {
     port: 8080,
     proxy: {
